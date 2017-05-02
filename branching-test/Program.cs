2e66a8da8a33a1bfd3cc2f360a52286e6d9f83bd
saveriogrisorio <saveriogrisorio@ehouse.co.uk>
2017-05-02 09:53:39 +0100

    Dev-change 3

--- branching-test/Program.cs
+++ branching-test/Program.cs
@@ -27,6 +27,9 @@ namespace branching_test
 
             Console.WriteLine("This is the second change in dev branch.");
             Console.ReadLine();
+
+            Console.WriteLine("This is the third change in dev branch.");
+            Console.ReadLine();
         }
     }
 }
