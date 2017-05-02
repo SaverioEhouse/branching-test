a000b29427c294f9080f0b4f46d8c9f2789fb220
saveriogrisorio <saveriogrisorio@ehouse.co.uk>
2017-05-02 12:21:38 +0100

    feature1-start

--- branching-test/Program.cs
+++ branching-test/Program.cs
@@ -8,10 +8,18 @@ namespace branching_test
 {
     class Program
     {
+        static void feature1()
+        {
+            Console.WriteLine("This is the feature1.");
+            Console.ReadLine();
+        }
+
         static void Main(string[] args)
         {
             Console.WriteLine("This is the master branch. Command one.");
             Console.ReadLine();
+
+            feature1();
         }
     }
 }
