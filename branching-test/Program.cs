88ba38fd51bb03400b3a0125a2e9965a02d2de8b
saveriogrisorio <saveriogrisorio@ehouse.co.uk>
2017-05-02 09:45:40 +0100

    Reset master branch

--- branching-test/Program.cs
+++ branching-test/Program.cs
@@ -12,21 +12,6 @@ namespace branching_test
         {
             Console.WriteLine("This is the master branch. Command one.");
             Console.ReadLine();
-
-            Console.WriteLine("This is the dev branch. Command two.");
-            Console.ReadLine();
-
-            Console.WriteLine("This is coming from the Feature-1.");
-            Console.ReadLine();
-
-            Console.WriteLine("This is still coming from the Feature-1. Commit 2.");
-            Console.ReadLine();
-
-            Console.WriteLine("This change is done in dev branch. No further branch was used.");
-            Console.ReadLine();
-
-            Console.WriteLine("This is the second change in dev branch.");
-            Console.ReadLine();
         }
     }
 }
