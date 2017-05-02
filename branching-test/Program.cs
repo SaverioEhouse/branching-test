77e0a16a7ec6f8523b10d1268d26f2c0a9d4b071
saveriogrisorio <saveriogrisorio@ehouse.co.uk>
2017-05-02 11:13:53 +0100

    master - reset

--- branching-test/Program.cs
+++ branching-test/Program.cs
@@ -13,53 +13,7 @@ namespace branching_test
             Console.WriteLine("This is the master branch. Command one.");
             Console.ReadLine();
 
-            #region dev
-            Console.WriteLine("This is the dev branch. Command two.");
-            Console.ReadLine();
-
-            Console.WriteLine("This is coming from the Feature-1.");
-            Console.ReadLine();
-
-            Console.WriteLine("This is still coming from the Feature-1. Commit 2.");
-            Console.ReadLine();
-
-            Console.WriteLine("This change is done in dev branch. No further branch was used.");
-            Console.ReadLine();
-
-            Console.WriteLine("This is the second change in dev branch.");
-            Console.ReadLine();
-
-            Console.WriteLine("This is the third change in dev branch.");
-            Console.ReadLine();
-            #endregion
-
-            #region dev-saverio
-
-            Console.WriteLine("Saverio change 1");
-            Console.ReadLine();
-
-            Console.WriteLine("Saverio change 2");
-            Console.ReadLine();
-
-            #endregion
-
-            Console.WriteLine("Hotfix-1 - master");
-            Console.ReadLine();
-
-            Console.WriteLine("Hotfix-2 - master");
-            Console.ReadLine();
-
-            Console.WriteLine("Dev is going on....");
-            Console.ReadLine();
-
-            Console.WriteLine("...and on...");
-            Console.ReadLine();
-
-            Console.WriteLine("...still going on...");
-            Console.ReadLine();
-
-            Console.WriteLine("Dev is ready for going live");
-            Console.ReadLine();
+            
         }
     }
 }
